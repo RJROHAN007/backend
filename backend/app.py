e021a131adf3a59369131f1976281f84e6575363
soumyajit maity <rohan1907rohan@outlook.com>
2025-10-03 20:20:36 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -427,5 +427,6 @@ async def health_check():
     }
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8000))  # Use host PORT if provided
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
